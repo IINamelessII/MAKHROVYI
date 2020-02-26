d54f4ae3a32c045c77f528b3d37fde52a9045e8f
AxiAxi <mhubstudy@gmail.com>
2020-02-26 17:03:09 +0300

    Init FileBrowser and replace filters with it

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,9 +5,9 @@ import asyncComponent from './hoc/asyncComponent/asyncComponent';
 import NavigationBar from './containers/NavigationBar/NavigationBar';
 import Screen from './containers/Screen/Screen';
 
-const asyncSolutions = asyncComponent(() => {
-  return import('./containers/Solutions/Solutions');
-});
+// const asyncSolutions = asyncComponent(() => {
+//   return import('./containers/Solutions/Solutions');
+// });
 
 const asyncTutorials = asyncComponent(() => {
   return import('./containers/Tutorials/Tutorials');
@@ -17,13 +17,17 @@ const asyncUpload = asyncComponent(() => {
   return import('./containers/Upload/Upload');
 });
 
+const asyncFileBrowser = asyncComponent(() => {
+  return import('./containers/FileBrowser/FileBrowser');
+})
+
 class App extends Component {
   render () {
     let routes = (
       <Switch>
         <Route path="/tutorials" component={asyncTutorials} />
         <Route path="/upload" component={asyncUpload} />
-        <Route path="/" exact component={asyncSolutions} />
+        <Route path="/" exact component={asyncFileBrowser} />
         <Redirect to="/" />
       </Switch>
     )
---
+++ frontend/src/containers/FileBrowser/FileBrowser.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+class FileBrowser extends Component {
+  render() {
+    return (
+      <div>FB</div>
+    );
+  }
+}
+
+export default FileBrowser;
