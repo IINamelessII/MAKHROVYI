ebf60198c3f6feb8eea6ecf001db0b77611df40e
Dmytrii Puzyr <slymahel@gmail.com>
2020-02-29 18:04:29 +0300

    Add Screen styling

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -37,6 +37,7 @@ class App extends Component {
           {routes}
           <Backdrop />
         </Screen>
+        <div></div>
       </Layout>
     );
   }
--- frontend/src/containers/Screen/Screen.js
+++ frontend/src/containers/Screen/Screen.js
@@ -1,9 +1,11 @@
 import React, {Component} from 'react';
 
+import classes from './Screen.css';
+
 class Screen extends Component {
   render() {
     return (
-      <div>
+      <div className={classes.Screen}>
         {this.props.children}
       </div>
     );
