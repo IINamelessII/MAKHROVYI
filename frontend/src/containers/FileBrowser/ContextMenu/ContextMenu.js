713977c9509cbc9064ea0ec431beaf2d2b92d4b7
Oleh Serikov <Oleh.Serikov@protonmail.com>
2020-03-04 13:27:15 +0300

    Change ContextMenu behavior

--- frontend/src/containers/FileBrowser/ContextMenu/ContextMenu.js
+++ frontend/src/containers/FileBrowser/ContextMenu/ContextMenu.js
@@ -1,21 +1,45 @@
-import React from 'react';
+import React, {Component} from 'react';
 
-const contextMenu = (props) => {
-  const options = props.options.map((option) => {
+import classes from './ContextMenu.css';
+
+class ContextMenu extends Component {
+  state = {
+    x: 0,
+    y: 0,
+  }
+
+  moveContextMenu = (event) => {
+    const x = event.clientX;
+    const y = event.clientY;
+    this.setState({
+      x: x,
+      y: y,
+    });
+  }
+
+  render() {
+    //TODO add Redux action, that get coords from root(App) and add styling in ComponentWillRender
+
+    const options = this.props.options.map((option) => {
+      return (
+        <div
+          key={option.label}
+          onClick={option.action}
+          className={classes.ContextMenu}>{option.label}</div>
+      );
+    })
+
     return (
-      <div
-        key={option.label}
-        onClick={option.action}>{option.label}</div>
+      <div 
+        className={classes.Option}
+        onMouseMove={this.moveContextMenu}
+        style={{top: this.state.y + 'px', left:this.state.x + 'px',}}>
+        {options}
+      </div>
     );
-  })
-
-  return (
-    <div>
-      {options}
-    </div>
-  );
+  }
 }
 
-export default contextMenu;
+export default ContextMenu;
 
 //TODO: Add Context menu over backdrop
