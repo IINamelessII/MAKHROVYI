bdd75a1cb2ce8c06dd55bd0a5f89c80107aab4e7
Oleh Serikov <Oleh.Serikov@protonmail.com>
2020-03-05 10:53:38 +0300

    Add mouse tracking to Backdrop

--- frontend/src/hoc/Layout/Backdrop/Backdrop.js
+++ frontend/src/hoc/Layout/Backdrop/Backdrop.js
@@ -6,12 +6,19 @@ import * as actions from '../../../store/actions/index';
 import classes from './Backdrop.css';
 
 class Backdrop extends Component {
+
+  onMouseMove = (event) => {
+    const x = event.pageX;
+    const y = event.pageY;
+    this.props.updatePosition(x, y);
+  }
   
   render() {
     return this.props.show ? (
       <div 
         onClick={this.props.onBackdropHide} 
-        className={classes.Backdrop}>
+        className={classes.Backdrop}
+        onMouseMove={(event) => this.onMouseMove(event)}>
       </div>
     ) : null;
   }
@@ -26,6 +33,7 @@ const mapStateToProps = state => {
 const mapDispatchToProps = dispatch => {
   return {
     onBackdropHide: () => dispatch(actions.hideBackdrop()),
+    updatePosition: (x, y) => dispatch(actions.getPostion(x, y)),
   };
 };
 
