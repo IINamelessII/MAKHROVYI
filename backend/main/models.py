93ee01219aad55592375ab40e2da580f53a14fac
Dmytrii Puzyr <slymahel@gmail.com>
2020-03-11 17:44:57 +0300

    Add downloads and created_date fields to Fils and Dirs

--- backend/main/models.py
+++ backend/main/models.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.utils.timezone import now
 
 
 class User(AbstractUser):
@@ -10,6 +11,8 @@ class Dir(models.Model):
     name = models.CharField(max_length=100)
     dirs = models.ManyToManyField('Dir', blank=True)
     files = models.ManyToManyField('File', blank=True)
+    downloads = models.IntegerField(default=0)
+    created_date = models.DateTimeField(default=now, editable=False)
 
     def __str__(self):
         return '#{} {}'.format(self.id, self.name)
@@ -19,6 +22,8 @@ class File(models.Model):
     name = models.CharField(max_length=100)
     ext = models.CharField(max_length=10)
     file = models.FileField()
+    downloads = models.IntegerField(default=0)
+    created_date = models.DateTimeField(default=now, editable=False)
 
     def __str__(self):
         return '#{} {}'.format(self.id, self.name, '.', self.ext)
--- buildrun.sh
+++ buildrun.sh
@@ -1,4 +1,7 @@
+#!/bin/bash
+git pull
 cd frontend
 npm run build
 cd ../
+source backend/venv/bin/activate
 python backend/manage.py runserver
