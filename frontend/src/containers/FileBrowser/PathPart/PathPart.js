fd354690de7bd5faa6085c6dc533c6b981b9c0f1
Dmytrii Puzyr <slymahel@gmail.com>
2020-03-19 16:08:02 +0300

    Add scroll for PathRow

--- frontend/src/containers/FileBrowser/PathPart/PathPart.js
+++ frontend/src/containers/FileBrowser/PathPart/PathPart.js
@@ -3,12 +3,11 @@ import React from 'react';
 import classes from './PathPart.css';
 
 const pathPart = (props) => {
-  //TODO Remove nested span and move ::after
   return (
-    <span className={classes.PathPart}>
-      <span onClick={props.goToPath}>{props.pathPartName}</span>
+    <div className={classes.PathPart}>
+      <div onClick={props.goToPath}>{props.pathPartName}</div>
       {props.pathPartName !== '~/' ? "/" : ''}
-    </span>
+    </div>
   );
 }
 
