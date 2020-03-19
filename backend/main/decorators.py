b145f518fae1330248febac4470bc02e200565a0
RedBullDogE <gsb06092000@gmail.com>
2020-03-19 15:33:37 +0300

    Move some reusable view logic to decorators

---
+++ backend/main/decorators.py
@@ -0,0 +1,27 @@
+import json
+from django.http import HttpResponse
+
+
+def load_data(*keys):
+    def load_data_decorator(func):
+        def load_data_wrapper(request, *args, **kwargs):
+            try:
+                raw = json.loads(request.body.decode('utf-8'))
+                data = dict()
+                for key in keys:
+                    data[key] = raw[key]
+            except:
+                return HttpResponse(status=404)
+            else:
+                return func(request, data, *args, **kwargs)
+        return load_data_wrapper
+    return load_data_decorator
+
+
+def post_only(func):
+    def post_only_wrapper(request, *args, **kwargs):
+        if request.method == 'POST':
+            return func(request, *args, **kwargs)
+        else:
+            return HttpResponse(status=400)
+    return post_only_wrapper
--- backend/main/views.py
+++ backend/main/views.py
@@ -3,12 +3,14 @@ import os
 import shutil
 import time
 import zipfile
-from pprint import pprint #For dev purposes only TODO:Remove the import and uses
 from secrets import token_urlsafe
+
 from django.conf import settings
 from django.http import HttpResponse
 from django.shortcuts import render
 from rest_framework import viewsets
+
+from main.decorators import load_data, post_only
 from main.forms import UploadFileForm
 from main.models import Dir, File
 from main.permissions import IsReadOnly
@@ -34,20 +36,17 @@ def index(request):
     return render(request, 'index.html')
 
 
-def download(request):
+@post_only
+@load_data('id')
+def download(request, data):
     """return file link by its id"""
-    try:
-        data = json.loads(request.body.decode('utf-8'))
-        the_file_record = File.objects.get(pk=data['id'])
-    except:
-        return HttpResponse(status=404)
-    else:
-        filename = os.path.basename(the_file_record.file.name)
-        response = HttpResponse(settings.MEDIA_URL + filename)
-        response['Content-Disposition'] = 'attachment; filename={}'.format(filename)
+    the_file_record = File.objects.get(pk=data['id'])
+    filename = os.path.basename(the_file_record.file.name)
+    response = HttpResponse(settings.MEDIA_URL + filename)
+    response['Content-Disposition'] = 'attachment; filename={}'.format(filename)
 
-        the_file_record.inc_download()
-        return response
+    the_file_record.inc_download()
+    return response
 
 
 def prepare_dir(id, space):
@@ -70,147 +69,134 @@ def prepare_dir(id, space):
         prepare_dir(the_dir.id, next_dir_path)
        
 
-def archive(request):
+@post_only
+@load_data('id')
+def archive(request, data):
     """create archive from dir and return its link by dir's id"""
-    try:
-        data = json.loads(request.body.decode('utf-8'))
-        the_dir_record = Dir.objects.get(pk=data['id'])
-        #TODO add oportunity to create zip/tar.xz archives based on archive_type param
-    except:
-        return HttpResponse(status=404)
-    else:
+    the_dir_record = Dir.objects.get(pk=data['id'])
+    #TODO add oportunity to create zip/tar.xz archives based on archive_type param
+    token = token_urlsafe(16)
+    path_with_token = os.path.join(settings.ARCHIVES_ROOT, token)
+    while os.path.exists(path_with_token):
         token = token_urlsafe(16)
         path_with_token = os.path.join(settings.ARCHIVES_ROOT, token)
-        while os.path.exists(path_with_token):
-            token = token_urlsafe(16)
-            path_with_token = os.path.join(settings.ARCHIVES_ROOT, token)
-        
-        os.mkdir(path_with_token)
     
-        prepare_dir(data['id'], path_with_token)
+    os.mkdir(path_with_token)
 
-        shutil.make_archive(path_with_token, 'zip', path_with_token)
+    prepare_dir(data['id'], path_with_token)
 
-        response = HttpResponse(settings.ARCHIVES_URL + token + '.zip')
-        response['Content-Disposition'] = 'attachment; filename={}'.format(token)
+    shutil.make_archive(path_with_token, 'zip', path_with_token)
+
+    response = HttpResponse(settings.ARCHIVES_URL + token + '.zip')
+    response['Content-Disposition'] = 'attachment; filename={}'.format(token)
+    
+    return response
         
-        return response
-        
 
-def archive_received(request):
+@post_only
+@load_data('token')
+def archive_received(request, data):
     """Removing archive with given token after 1 hour after creating"""
+    time.sleep(settings.TIME_TO_DELETE)
+    archive_path = os.path.join(settings.ARCHIVES_ROOT, data['token'])
     try:
-        data = json.loads(request.body.decode('utf-8'))
-        token = data['token']
-    except:
-        return HttpResponse(status=404)
-    else:
-        time.sleep(settings.TIME_TO_DELETE)
-        archive_path = os.path.join(settings.ARCHIVES_ROOT, token)
         shutil.rmtree(archive_path)
         os.remove(archive_path + '.zip')
-        return HttpResponse(status=200)
-
-
-def upload_file(request, id):
-    """Upload file to dir with given id"""
-    if request.method == 'POST':
-        try:
-            the_file = request.FILES['file']
-            last_dot_index = the_file.name.rfind('.')
-            instance = File(
-                file=the_file, 
-                name=the_file.name[:last_dot_index],
-                ext=the_file.name[last_dot_index + 1:],
-                mmtype=the_file.content_type,
-            )
-            instance.save()
-            parentDir = Dir.objects.get(pk=id)
-            parentDir.files.add(File.objects.get(pk=instance.id))
-            return HttpResponse(status=200)
-        except:
-            return HttpResponse(status=401)
-    else:
-        return HttpResponse(status=400)
-
-
-def add_new_dir(request):
-    """Add New Directory with given name to the directory with the given dirId"""
-    try:
-        data = json.loads(request.body.decode('utf-8'))
-        dirId = data['dirId']
-        value = data['value']
     except:
         return HttpResponse(status=404)
-    else:
-        instance = Dir(name=value)
+    return HttpResponse(status=200)
+
+
+@post_only
+def upload_file(request, id):
+    """Upload file to dir with given id"""
+    try:
+        the_file = request.FILES['file']
+        last_dot_index = the_file.name.rfind('.')
+        instance = File(
+            file=the_file, 
+            name=the_file.name[:last_dot_index],
+            ext=the_file.name[last_dot_index + 1:],
+            mmtype=the_file.content_type,
+        )
         instance.save()
-        parentDir = Dir.objects.get(pk=dirId)
-        parentDir.dirs.add(Dir.objects.get(pk=instance.id))
+        parentDir = Dir.objects.get(pk=id)
+        parentDir.files.add(File.objects.get(pk=instance.id))
         return HttpResponse(status=200)
+    except:
+        return HttpResponse(status=401)
 
 
+@post_only
+@load_data('dirId', 'value')
+def add_new_dir(request, data):
+    """Add New Directory with given name to the directory with the given dirId"""
+    instance = Dir(name=data['value'])
+    instance.save()
+    parentDir = Dir.objects.get(pk=data['dirId'])
+    parentDir.dirs.add(Dir.objects.get(pk=instance.id))
+    return HttpResponse(status=200)
+
+
+@post_only
 def upload_dir(request, id):
     """Upload dir to dir with given id"""
-    if request.method == 'POST':
-        files = request.FILES.getlist('file')
-        relpaths = json.load(request.FILES['relPaths'].file)
-        #TODO: Move this function to Dir model
-        paths = [path.split('/')[:-1] for path in relpaths]
-        #file_dir_ids is a list of dir's ids where files located
-        file_dir_ids = []
+    files = request.FILES.getlist('file')
+    relpaths = json.load(request.FILES['relPaths'].file)
+    #TODO: Move this function to Dir model
+    paths = [path.split('/')[:-1] for path in relpaths]
+    #file_dir_ids is a list of dir's ids where files located
+    file_dir_ids = []
+    
+    dir_inst = Dir(name=paths[0][0])
+    dir_inst.save()
+    d = [{
+        dir_inst.id: {
+            'name': dir_inst.name,
+            'cont': []
+        }
+    }]
+    Dir.objects.get(pk=id).dirs.add(Dir.objects.get(pk=dir_inst.id))
+    saved_id = dir_inst.id #Currently root id
+
+    for path in paths:
+        d_link = d #Linking object behind d variable
+        for part in path:
+            id = 0
+            # for key, value in d_link.items():
+            for index, i in enumerate(d_link):
+                key, value = list(i.items())[0]
+                if value['name'] == part:
+                    id = key
+                    break
+            if id: #if part was finded
+                d_link = d_link[index][id]['cont']
+                saved_id = id
+            else: #Create new Dir Record
+                dir_inst = Dir(name=part)
+                dir_inst.save()
+                Dir.objects.get(pk=saved_id).dirs.add(Dir.objects.get(pk=dir_inst.id))
+                d_link.append({dir_inst.id: {
+                    'name': part,
+                    'cont': []
+                }})
+                d_link = d_link[-1][dir_inst.id]['cont']
+                saved_id = dir_inst.id
         
-        dir_inst = Dir(name=paths[0][0])
-        dir_inst.save()
-        d = [{
-            dir_inst.id: {
-                'name': dir_inst.name,
-                'cont': []
-            }
-        }]
-        Dir.objects.get(pk=id).dirs.add(Dir.objects.get(pk=dir_inst.id))
-        saved_id = dir_inst.id #Currently root id
+        file_dir_ids.append(saved_id)
+                    
+    for id, the_file in enumerate(files):
+        #Creating File record
+        last_dot_index = the_file.name.rfind('.')
+        instance = File(
+            file=the_file, 
+            name=the_file.name[:last_dot_index],
+            ext=the_file.name[last_dot_index + 1:],
+            mmtype=the_file.content_type,
+        )
+        instance.save()
+        #Adding File to the parent Dir
+        parent_dir = Dir.objects.get(pk=file_dir_ids[id])
+        parent_dir.files.add(File.objects.get(pk=instance.id))
 
-        for path in paths:
-            d_link = d #Linking object behind d variable
-            for part in path:
-                id = 0
-                # for key, value in d_link.items():
-                for index, i in enumerate(d_link):
-                    key, value = list(i.items())[0]
-                    if value['name'] == part:
-                        id = key
-                        break
-                if id: #if part was finded
-                    d_link = d_link[index][id]['cont']
-                    saved_id = id
-                else: #Create new Dir Record
-                    dir_inst = Dir(name=part)
-                    dir_inst.save()
-                    Dir.objects.get(pk=saved_id).dirs.add(Dir.objects.get(pk=dir_inst.id))
-                    d_link.append({dir_inst.id: {
-                        'name': part,
-                        'cont': []
-                    }})
-                    d_link = d_link[-1][dir_inst.id]['cont']
-                    saved_id = dir_inst.id
-            
-            file_dir_ids.append(saved_id)
-                     
-        for id, the_file in enumerate(files):
-            #Creating File record
-            last_dot_index = the_file.name.rfind('.')
-            instance = File(
-                file=the_file, 
-                name=the_file.name[:last_dot_index],
-                ext=the_file.name[last_dot_index + 1:],
-                mmtype=the_file.content_type,
-            )
-            instance.save()
-            #Adding File to the parent Dir
-            parent_dir = Dir.objects.get(pk=file_dir_ids[id])
-            parent_dir.files.add(File.objects.get(pk=instance.id))
-
-        return HttpResponse(status=200)
-    else:
-        return HttpResponse(status=400)
+    return HttpResponse(status=200)
