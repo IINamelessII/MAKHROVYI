ef36a311d5b59c17b67939f65b54056a9dd2b129
RedBullDogE <gsb06092000@gmail.com>
2020-03-21 16:55:42 +0300

    Add serving all urls, not specified for back to front

--- backend/main/urls.py
+++ backend/main/urls.py
@@ -9,11 +9,11 @@ router.register(r'files', views.FileViewSet)
 
 urlpatterns = [
     path('api/', include(router.urls)),
-    path('', views.index, name='index'),
     path('download/', views.download, name='download'),
     path('archive/', views.archive, name='archive'),
     path('archive_received/', views.archive_received, name='archive_received'),
     path('upload_file/<int:id>/', views.upload_file, name='upload_file'),
     path('newdir/', views.add_new_dir, name='add_new_dir'),
     path('upload_dir/<int:id>/', views.upload_dir, name='upload_dir'),
+    re_path(r'^.*', views.index, name='index'), #All another urls served by front
 ]
