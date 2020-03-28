4ffbb5986345108d316dad1c41f189793483459b
Dmytrii Puzyr <slymahel@gmail.com>
2020-03-28 16:45:17 +0300

    Add Rules to Info

--- frontend/src/containers/Info/Info.js
+++ frontend/src/containers/Info/Info.js
@@ -5,8 +5,14 @@ import classes from './Info.css';
 class Info extends Component {
   render() {
     return (
-      <div className={classes.Info}> 
-        INFO
+      <div className={classes.Info}>
+        <div className={classes.Rules}>
+          <p className={classes.Rule}>Everybody can download solutions as separated files or whole directories.</p>
+          <p className={classes.Rule}>Everybody can upload files and create directories after authentication.</p>
+          <p className={classes.Rule}>Please respect other users and do not duplicate existing directories.</p>
+          {/* <p className={classes.Rule}></p>
+          <p className={classes.Rule}></p> */}
+        </div> 
       </div>
     );
   }
