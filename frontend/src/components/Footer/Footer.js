4e73fbf30ebe93b5bb97141718cd4bd1c9992fc9
RedBullDogE <gsb06092000@gmail.com>
2020-04-03 15:05:43 +0300

    Update license link in the footer

--- frontend/src/components/Footer/Footer.js
+++ frontend/src/components/Footer/Footer.js
@@ -9,13 +9,13 @@ const footer = props => {
       <span className={classes.Label}>© 2019 Oleh Serikov, Inc</span>
       <Media query="(max-width: 600px)" render={() => (
         <div className={classes.Container}>
-          <span className={classes.Label}><a href="https://github.com/IINamelessII/MAKHROVYI">License</a></span>
+          <span className={classes.Label}><a href="https://github.com/IINamelessII/MAKHROVYI/blob/master/LICENSE">License</a></span>
           <span className={classes.Label}><a href="/info">Information</a></span>
         </div>
       )} />
       <Media query="(min-width: 601px)" render={() => (
         <React.Fragment>
-          <span className={classes.Label}><a href="https://github.com/IINamelessII/MAKHROVYI">License</a></span>
+          <span className={classes.Label}><a href="https://github.com/IINamelessII/MAKHROVYI/blob/master/LICENSE">License</a></span>
           <span className={classes.Label}><a href="/info">Information</a></span>
         </React.Fragment>
       )} />
