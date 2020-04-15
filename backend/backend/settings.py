0d0ad8a27a8cecfbda16f7a6ca9e9482b40a871b
RedBullDogE <gsb06092000@gmail.com>
2020-04-16 00:18:56 +0300

    Docker changes

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -137,8 +137,8 @@ DATABASES = {
 }
 
 # Celery
-CELERY_BROKER_URL = 'redis://localhost:6379'
-CELERY_RESULT_BACKEND = 'redis://localhost:6379'
+CELERY_BROKER_URL = 'redis://redis:6379'
+CELERY_RESULT_BACKEND = 'redis://redis:6379'
 CELERY_ACCEPT_CONTENT = ['application/json']
 CELERY_RESULT_SERIALIZER = 'json'
 CELERY_TASK_SERIALIZER = 'json'
