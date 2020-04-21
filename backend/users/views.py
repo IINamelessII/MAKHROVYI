231a8d466b78523a906d2261df5a976c22a89934
RedBullDogE <gsb06092000@gmail.com>
2020-04-21 16:48:37 +0300

    Fix FileUploadInput responsive styles

--- backend/users/views.py
+++ backend/users/views.py
@@ -4,6 +4,7 @@ from django.http import HttpResponse
 
 
 def user_data(request):
+    return HttpResponse(json.dumps({'photo': 'https://via.placeholder.com/150', 'id': 1, 'name': 'debug'}), status=200) #DEBUGONLY
     """Return cureent user photo and id if user is authenticated via OAuth"""
     if hasattr(request.user, 'socialaccount_set') and len(request.user.socialaccount_set.all()):
         data = {
